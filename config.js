3ad936b6331b930a988f3ed5215f3df812ae33ff
Harsh Vardhan <hcvardhan0917@gmail.com>
2026-06-28 22:12:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
-// config.js - Public Mock Configuration
 export const firebaseConfig = {
-  apiKey: "MOCK_FIREBASE_KEY_FOR_EVALUATION", 
-  authDomain: "crunchtime-mock.firebaseapp.com",
-  projectId: "crunchtime-mock",
-  storageBucket: "crunchtime-mock.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:0000000000000000000000" 
+  apiKey: "AIzaSyDxval4eTEYGPttDaEF2PrKjaCi6Z9ZABM",
+  authDomain: "crunchtime-57e84.firebaseapp.com",
+  projectId: "crunchtime-57e84",
+  storageBucket: "crunchtime-57e84.firebasestorage.app",
+  messagingSenderId: "220460702653",
+  appId: "1:220460702653:web:a6193a97e389999b6a50d4"
 };
 
-export const GEMINI_API_KEY = "MOCK_GEMINI_KEY_FOR_EVALUATION";
+export const GEMINI_API_KEY = "API_KEy";
