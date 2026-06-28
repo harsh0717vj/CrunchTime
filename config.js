9b484557e03a684421da189cb0c473e93e8e8026
Harsh Vardhan <hcvardhan0917@gmail.com>
2026-06-28 23:26:06 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDxval4eTEYGPttDaEF2PrKjaCi6Z9ZABM",
+  authDomain: "crunchtime-57e84.firebaseapp.com",
+  projectId: "crunchtime-57e84",
+  storageBucket: "crunchtime-57e84.firebasestorage.app",
+  messagingSenderId: "220460702653",
+  appId: "1:220460702653:web:a6193a97e389999b6a50d4"
+};
+
+export const GEMINI_API_KEY = "AQ.Ab8RN6LKNJOWxp6xVxC5sHOVaEAaHQoJs12C9qIXCWJqP8b1Qg";
